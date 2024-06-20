9812d9d10a6117ea649f11b8d7be0a2aad7052af
Less-parrot <less100100@gmail.com>
2024-06-20 15:24:19 -0500

    update: add items for room

--- feature-profile/src/main/java/org/compose_projects/socialLocal/feature/profile/ui/testRoomAndHilt/components.kt
+++ feature-profile/src/main/java/org/compose_projects/socialLocal/feature/profile/ui/testRoomAndHilt/components.kt
@@ -27,8 +27,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
+import org.compose_projects.socialLocal.core.data.data.ChatProvider
 import org.compose_projects.socialLocal.core.data.data.DataChatProvider
 import org.compose_projects.socialLocal.core.data.data.MultimediaProvider
+import org.compose_projects.socialLocal.core.data.data.ProfileProvider
+import org.compose_projects.socialLocal.core.data.data.UserProvider
 
 @Composable
 internal fun Liner(content: @Composable () -> Unit) {
@@ -116,9 +119,86 @@ private fun ContentAdd(
         .height(50.dp)
 
     when (entity) {
-        chatEntity -> {}
-        profileEntity -> {}
-        userEntity -> {}
+        chatEntity -> {
+
+            var isChatGlobal by remember { mutableStateOf("true")}
+
+            Column {
+                TextField(
+                    value = isChatGlobal,
+                    onValueChange = { isChatGlobal = it },
+                    modifier = modifier
+                )
+
+                Button(onClick = {
+                    testRoomAndHiltViewModel.insertChat(
+                        ChatProvider(
+                            isChatGlobal = if (isChatGlobal == "true") true else false
+                        )
+                    )
+                }) {
+                    Text(text = "Add items for $entity")
+                }
+            }
+
+        }
+        profileEntity -> {
+
+            var pathImageProfile by remember { mutableStateOf("") }
+            var description by remember { mutableStateOf("") }
+
+            Column {
+                TextField(
+                    value = pathImageProfile,
+                    onValueChange = { pathImageProfile = it },
+                    modifier = modifier
+                )
+                TextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    modifier = modifier
+                )
+
+                Button(onClick = {
+                    testRoomAndHiltViewModel.insertProfile(
+                        ProfileProvider(
+                            pathImageProfile = pathImageProfile,
+                            description = description
+                        )
+                    )
+                }) {
+                    Text(text = "Add items for $entity")
+                }
+            }
+        }
+
+        userEntity -> {
+            var iAm by remember { mutableStateOf("true") }
+            var isFriend by remember { mutableStateOf("true") }
+            Column {
+                TextField(
+                    value = iAm,
+                    onValueChange = { iAm = it },
+                    modifier = modifier
+                )
+                TextField(
+                    value = isFriend,
+                    onValueChange = { isFriend = it },
+                    modifier = modifier
+                )
+                Button(onClick = {
+                    testRoomAndHiltViewModel.insertUser(
+                        UserProvider(
+                            iAm = if (iAm == "true") true else false,
+                            isFriend = if (isFriend == "true") true else false
+                        )
+                    )
+                }) {
+                    Text(text = "Add items for $entity")
+                }
+            }
+        }
+
         dataChatEntity -> {
 
             var dateTime by remember { mutableStateOf("") }
@@ -142,13 +222,15 @@ private fun ContentAdd(
                     modifier = modifier
                 )
 
-                Button(onClick = { testRoomAndHiltViewModel.insertDataChat(
-                    DataChatProvider(
-                        dateTime = dateTime,
-                        iSend = if (iSend == "true") true else false,
-                        contentType = contentType
+                Button(onClick = {
+                    testRoomAndHiltViewModel.insertDataChat(
+                        DataChatProvider(
+                            dateTime = dateTime,
+                            iSend = if (iSend == "true") true else false,
+                            contentType = contentType
+                        )
                     )
-                ) }) {
+                }) {
                     Text(text = "Add items for $entity")
                 }
 
