32fc1d0637bd6853a11cc0ad782b8c1697e58c37
Less-parrot <less100100@gmail.com>
2024-06-25 14:28:27 -0500

    update: SLPreferences and its respective viewModel in data module

--- core-ui/src/main/java/org/compose_projects/socialLocal/core/ui/colorPreferences/SLChangeTheme.kt
+++ core-ui/src/main/java/org/compose_projects/socialLocal/core/ui/colorPreferences/SLChangeTheme.kt
@@ -1,12 +1,6 @@
 package org.compose_projects.socialLocal.core.ui.colorPreferences
 
-import android.widget.Toast
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,12 +8,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.compose_projects.socialLocal.core.data.common.states.SLPreferencesState
 import org.compose_projects.socialLocal.core.data.common.themes
 import org.compose_projects.socialLocal.core.data.common.viewModels.SLPreferencesViewModel
-import org.compose_projects.socialLocal.core.data.data.SLPreferencesProvider
 
 @Composable
 fun SLChangeTheme(slPreferencesViewModel: SLPreferencesViewModel = hiltViewModel()) {
@@ -51,13 +42,3 @@ fun SLChangeTheme(slPreferencesViewModel: SLPreferencesViewModel = hiltViewModel
 }
 
 
-@Composable
-fun RevData(data: List<SLPreferencesProvider>) {
-    data.map {
-        when(it.theme) {
-            themes.DEFAULT -> SLColor.value = SLThemesList.default
-            themes.BLACK -> SLColor.value = SLThemesList.black
-            else -> SLColor.value = SLThemesList.default
-        }
-    }
-}
